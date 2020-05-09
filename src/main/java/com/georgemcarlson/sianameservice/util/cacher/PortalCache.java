2465fe2a30af535bd790e2c5080bdd094b4846e1
George Carlson <gcarlson@adc4gis.com>
2020-05-09 11:55:18 -0500

    Fix Hardcoded Portals

--- src/main/java/com/georgemcarlson/sianameservice/util/cacher/PortalCache.java
+++ src/main/java/com/georgemcarlson/sianameservice/util/cacher/PortalCache.java
@@ -2,15 +2,15 @@ package com.georgemcarlson.sianameservice.util.cacher;
 
 public class PortalCache {
   public static final String[] PORTALS = new String[]{
-      "SiaSky.net",
-      "SiaCDN.com",
-      "Tutemwesi.com",
-      "SkyDrain.net",
-      "SiaLoop.net",
-      "SkyPortal.xyz",
-      "Luxor.tech",
-      "SkynetHub.io",
-      "UTXO.no",
-      "LightspeedHosting.com"
+      "siasky.net",
+      "siacdn.com",
+      "skynet.tutemwesi.com",
+      "skydrain.net",
+      "sialoop.net",
+      "skyportal.xyz",
+      "skynet.luxor.tech",
+      "skynethub.io",
+      "skynet.utxo.no",
+      "vault.lightspeedhosting.com"
   };
 }
